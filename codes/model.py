6fb4ef5ddd9b2318b9ac4acb67440e705fb96c4d
rmwenzel <rmwenzel@uw.edu>
2019-10-29 19:35:38 -0700

    Up to SVR

--- codes/model.py
+++ codes/model.py
@@ -1,5 +1,7 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import seaborn as sns
 import os
 import sys
 import time
@@ -231,3 +233,177 @@ def build_model_data(hpdfs, data_names, response):
         model_data[data_name]['X_' + data_name + '_test'] = X_test
         model_data[data_name]['y_' + data_name + '_test'] = y_test
     return model_data
+
+
+def rmse(model, X_train, y_train, n_splits=5, shuffle=True,
+         random_state=None):
+    """Train and cv rmse of model."""
+    kf = KFold(n_splits=n_splits, shuffle=shuffle, random_state=random_state)
+
+    train_rmse = np.sqrt(mean_squared_error(model.predict(X_train), y_train))
+    cv_scores = - cross_val_score(estimator=model, X=X_train, y=y_train,
+                                  cv=kf, scoring='neg_mean_squared_error',
+                                  n_jobs=-1)
+    cv_rmse = np.sqrt(np.mean(cv_scores))
+    return train_rmse, cv_rmse
+
+
+def fit_default_models(model_data, random_state=None):
+    """Fit some regressors to all datasets."""
+    def_models = defaultdict(dict)
+    for data_name in model_data:
+        models = {'lasso': Lasso(), 'ridge': Ridge(),
+                  'bayes_ridge': BayesianRidge(),
+                  'plsreg': PLSRegression(), 'svr': SVR(),
+                  'knn': KNeighborsRegressor(),
+                  'mlp': MLPRegressor(),
+                  'dec_tree': DecisionTreeRegressor(random_state=random_state
+                                                    ),
+                  'extra_tree': ExtraTreeRegressor(random_state=random_state),
+                  'xgb': XGBRegressor(objective='reg:squarederror',
+                                      random_state=random_state,
+                                      n_jobs=-1)}
+        for model in models:
+            X_train = model_data[data_name]['X_' + data_name + '_train']
+            y_train = model_data[data_name]['y_' + data_name + '_train']
+            def_models[data_name][model] = models[model].fit(X_train, y_train)
+    return def_models
+
+
+def model_comparison(fit_models, data_name, model_data,
+                     n_splits=5, shuffle=True, random_state=None):
+    """Train and cv rmse for all models on version of data."""
+    model_comp_df = pd.DataFrame(columns=['train_rmse', 'cv_rmse'],
+                                 index=list(fit_models[data_name].keys()))
+    for model_name in fit_models[data_name]:
+        X_train = model_data[data_name]['X_' + data_name + '_train']
+        y_train = model_data[data_name]['y_' + data_name + '_train']
+        model = fit_models[data_name][model_name]
+        train_rmse, cv_rmse = rmse(model, X_train, y_train, n_splits=10,
+                                   shuffle=True, random_state=None)
+        model_comp_df.loc[model_name, 'train_rmse'] = train_rmse
+        model_comp_df.loc[model_name, 'cv_rmse'] = cv_rmse
+    return model_comp_df
+
+
+def compare_performance(fit_models, model_data, n_splits=5,
+                        shuffle=True, random_state=None):
+    """Compare default models across datasets."""
+    comp_dfs = []
+    for data_name in model_data:
+        model_comp_df = model_comparison(fit_models, data_name, model_data,
+                                         n_splits=5, shuffle=True,
+                                         random_state=None)
+        comp_dfs += [model_comp_df]
+    comp_df = pd.concat(comp_dfs, axis=1, keys=list(model_data.keys()),
+                        names=['data', 'performance'])
+    comp_df = comp_df.reset_index().rename(columns={'index': 'model'})
+    return comp_df
+
+
+def plot_model_comp(comp_df, col, hue, **kwargs):
+    """Plot results of model comparison."""
+    df = comp_df.melt(id_vars='model')
+    g = sns.catplot(x='model', y='value', data=df, col=col,
+                    hue=hue, **kwargs)
+    g.set_xticklabels(rotation=90)
+
+
+def remove_models(models, drop_models):
+    """Drop models from consideration."""
+    copy = models.copy()
+    for data_name in copy:
+        for model in drop_models:
+            copy[data_name].pop(model)
+    return copy
+
+
+def ho_cv_rmse(search_params, X_train, y_train, fixed_params={},
+               est_name='bayes_ridge', n_splits=5, shuffle=True,
+               random_state=None):
+    """CV rmse objective function for hyperopt parameter search."""
+    if est_name == 'ridge':
+        est = Ridge(**{**search_params, **fixed_params})
+    elif est_name == 'bridge':
+        est = BayesianRidge(**{**search_params, **fixed_params})
+    elif est_name == 'pls':
+        est = PLSRegression(**{**search_params, **fixed_params})
+    elif est_name == 'svr':
+        est = SVR(**{**search_params, **fixed_params})
+    elif est_name == 'xgb':
+        est = XGBRegressor(**{**search_params, **fixed_params},
+                           objective='reg:squarederror',
+                           random_state=random_state, n_jobs=-1)
+    est.fit(X_train, y_train)
+    _, cv_rmse = rmse(model=est, X_train=X_train, y_train=y_train,
+                      n_splits=n_splits, shuffle=shuffle,
+                      random_state=random_state)
+    return cv_rmse
+
+
+def ho_results(obj, space, est_name, X_train, y_train,
+               fixed_params={}, max_evals=10,
+               n_splits=5, shuffle=True, random_state=None):
+    """Hyperopt parameter search results."""
+    fn = partial(obj, X_train=X_train, y_train=y_train,
+                 fixed_params=fixed_params, est_name=est_name,
+                 n_splits=n_splits, shuffle=shuffle,
+                 random_state=random_state)
+    trials = Trials()
+    params = fmin(fn=fn, space=space, algo=tpe.suggest, trials=trials,
+                  max_evals=max_evals)
+    return trials, params
+
+
+def rank_features(model, model_name, X_train, num_features,
+                  figsize=None, rotation=None):
+    """Plot most positive and negatively weighted features."""
+    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=figsize)
+
+    coef_df = pd.DataFrame({'feature': X_train.columns, 'coef': model.coef_},
+                           index=np.arange(X_train.shape[1]))
+
+    plt.subplot(1, 2, 1)
+    top_df = coef_df.sort_values(by='coef', ascending=False).head(num_features)
+    plt.title('Best ' + model_name + ' ' + str(num_features) +
+              ' most positive feature weights')
+    sns.barplot(x='feature', y='coef', data=top_df, palette='Greens_r')
+    plt.xticks(rotation=rotation)
+
+    plt.subplot(1, 2, 2)
+    bot_df = coef_df.sort_values(by='coef', ascending=False).tail(num_features)
+    plt.title('Best ' + model_name + ' ' + str(num_features) +
+              ' most negative feature weights')
+    sns.barplot(x='feature', y='coef', data=bot_df, palette='Reds_r')
+    plt.xticks(rotation=rotation)
+
+    fig.tight_layout()
+
+
+def compare_model_performance(fit_models, model_data, n_splits=5,
+                              shuffle=True, random_state=None):
+    """Compare train and cv rmse across datasets for single model."""
+    comp_df = pd.DataFrame(columns=['train_rmse', 'cv_rmse'])
+    for data_name in fit_models:
+        X_train = model_data[data_name]['X_' + data_name + '_train']
+        y_train = model_data[data_name]['y_' + data_name + '_train']
+        for model_name in fit_models[data_name]:
+            train_rmse, cv_rmse = rmse(fit_models[data_name][model_name],
+                                       X_train, y_train,
+                                       n_splits=n_splits, shuffle=shuffle,
+                                       random_state=random_state)
+            comp_df.loc[model_name, 'train_rmse'] = train_rmse
+            comp_df.loc[model_name, 'cv_rmse'] = cv_rmse
+
+    comp_df = comp_df.reset_index().rename(columns={'index': 'model'})
+    return comp_df
+
+
+def convert_to_int(ho_results, conv_params):
+    """Workaround for hyperopt search not returning ints."""
+    copy = ho_results.copy()
+    for param in conv_params:
+        copy = int(copy[param])
+
+
+    return copy
